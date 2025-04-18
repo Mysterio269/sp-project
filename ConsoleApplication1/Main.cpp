bf9b7cf141ab03ff9ef6a7c982677d3743187819
ahmed hossam <ah2005269@gmail.com>
2025-04-18 15:35:12 +0200

    Completed Main Menu with sound and background

--- ConsoleApplication1/Main.cpp
+++ ConsoleApplication1/Main.cpp
@@ -18,15 +18,18 @@ enum Gamestate
     leaderboard
 };
 Gamestate gamestate = mainmenu;
-Text StartGameText, SettingsText, ExitText;
+Text StartGameText, SettingsText, ExitText,LeaderboardText;
 Font defgamefont; // default game font
-Texture MainMenuBackground_Texture;
-Sprite MainMenuBackGround;
+Texture MainMenuButtons_Texture,MainMenuBackground_Texture;
+Sprite MainMenuButtons, MainMenuBackground;
 View view;
 Vector2i mouseScreenpos;
 Vector2f mouseWorldpos;
-RectangleShape StartButton(Vector2f(490, 110));
-FloatRect StartButtonBounds;
+RectangleShape StartButton(Vector2f(490, 110)),SettingsButton(Vector2f(490, 110)),LeaderboardButton(Vector2f(490, 110)),
+               ExitButton(Vector2f(490, 110));
+FloatRect StartButtonBounds,SettingsButtonBounds,LeaderboardButtonBounds,ExitButtonBounds;
+Sound MainMenuMusic;
+SoundBuffer MainMenuMusic_source;
 
 RenderWindow window(VideoMode(1280, 800), "Vampire Survivors Olympus");
 
@@ -38,6 +41,22 @@ void MainMenuButtonCheck()
     if (Mouse::isButtonPressed(Mouse::Left) && StartButtonBounds.contains(mouseWorldpos))
     {
         gamestate = gameloop;
+        MainMenuMusic.stop();
+    }
+    if (Mouse::isButtonPressed(Mouse::Left) && LeaderboardButtonBounds.contains(mouseWorldpos))
+    {
+        gamestate = leaderboard;
+        MainMenuMusic.stop();
+    }
+    if (Mouse::isButtonPressed(Mouse::Left) && SettingsButtonBounds.contains(mouseWorldpos))
+    {
+        gamestate = settings;
+        MainMenuMusic.stop();
+    }
+    if (Mouse::isButtonPressed(Mouse::Left) && ExitButtonBounds.contains(mouseWorldpos))
+    {
+        window.close();
+        MainMenuMusic.stop();
     }
 }
 float deltaTime;
@@ -95,28 +114,69 @@ int main()
     }
     return 0;
 }
+void MainmenuInit() {
+    //Main menu initializations
+    //please avoid editing this section
+    StartButton.setPosition(9853, 9818);
+    StartButton.setScale(0.6, 0.65);
+    StartButtonBounds = StartButton.getGlobalBounds();
+    StartGameText.setFont(defgamefont);
+    StartGameText.setString("Start game");
+    StartGameText.setPosition(9915, 9830);
+
+    SettingsButton.setPosition(9853, 9910);
+    SettingsButton.setFillColor(Color::Red);
+    SettingsButton.setScale(0.6, 0.65);
+    SettingsButtonBounds = SettingsButton.getGlobalBounds();
+    SettingsText.setFont(defgamefont);
+    SettingsText.setString("Settings");
+    SettingsText.setPosition(9930, 9923);
+
+    LeaderboardButton.setPosition(9853, 10002);
+    LeaderboardButton.setFillColor(Color::Red);
+    LeaderboardButton.setScale(0.6, 0.65);
+    LeaderboardButtonBounds = LeaderboardButton.getGlobalBounds();
+    LeaderboardText.setFont(defgamefont);
+    LeaderboardText.setString("Leaderboard");
+    LeaderboardText.setPosition(9900, 10015);
+
+    ExitButton.setPosition(9853, 10094);
+    ExitButton.setFillColor(Color::Red);
+    ExitButton.setScale(0.6, 0.65);
+    ExitButtonBounds = ExitButton.getGlobalBounds();
+    ExitText.setFont(defgamefont);
+    ExitText.setString("Exit");
+    ExitText.setPosition(9963, 10107);
+
+    MainMenuButtons_Texture.loadFromFile("Assets\\mainmenu.png");
+    MainMenuButtons.setTexture(MainMenuButtons_Texture);
+    MainMenuButtons.setOrigin(281, 325);
+    MainMenuButtons.setPosition(10000, 10000);
+    MainMenuButtons.setScale(0.6, 0.65);
+
+    MainMenuBackground_Texture.loadFromFile("Assets\\MainBackground.png");
+    MainMenuBackground.setTexture(MainMenuBackground_Texture);
+    MainMenuBackground.setScale(0.84, 1.12);
+    MainMenuBackground.setPosition(9340, 9300);
+
+    MainMenuMusic_source.loadFromFile("Assets\\MainMenuMusic.ogg");
+    MainMenuMusic.setBuffer(MainMenuMusic_source);
+    MainMenuMusic.play();
+    MainMenuMusic.setLoop(true);
+}
 void Start()
 {
     // code here is only executed at the start of the program
     // initializations of everything
     window.setFramerateLimit(60);
 
+    //Game font initialization
     defgamefont.loadFromFile("VampireZone.ttf");
-    StartGameText.setFont(defgamefont);
-    StartGameText.setString("Start game");
-    StartGameText.setPosition(9910, 9830);
 
-    StartButton.setPosition(9853, 9818);
-    StartButton.setScale(0.6, 0.65);
-    StartButtonBounds = StartButton.getGlobalBounds();
+    MainmenuInit();
+   
 
-    MainMenuBackground_Texture.loadFromFile("Assets\\mainmenu.png");
-    MainMenuBackGround.setTexture(MainMenuBackground_Texture);
-    MainMenuBackGround.setOrigin(281, 325);
-    MainMenuBackGround.setPosition(10000, 10000);
-    MainMenuBackGround.setScale(0.6, 0.65);
-
-    view.setCenter(10000, 10000);
+    view.setCenter(10000, 9800);
     window.setView(view);
 }
 
@@ -134,19 +194,46 @@ void Update()
     if (gamestate == gameloop)
     {
         // gameloop update
-        cout << "we are in game phase";
+        cout << "we are in game phase ";
         if (Keyboard::isKeyPressed(Keyboard::R))
         {
             gamestate = mainmenu;
+            MainMenuMusic.play();
+        }
+        if (Keyboard::isKeyPressed(Keyboard::Q))
+        {
+            gamestate = gameover;
         }
     }
     if (gamestate == settings)
     {
         // settings menu update
+        cout << "we are in settings menu ";
+        if (Keyboard::isKeyPressed(Keyboard::R))
+        {
+            gamestate = mainmenu;
+            MainMenuMusic.play();
+        }
+    }
+    if (gamestate == leaderboard)
+    {
+        // settings menu update
+        cout << "we are in leaderboard menu ";
+        if (Keyboard::isKeyPressed(Keyboard::R))
+        {
+            gamestate = mainmenu;
+            MainMenuMusic.play();
+        }
     }
     if (gamestate == gameover)
     {
         // gameover screen update
+        cout << "The player is dead ";
+        if (Keyboard::isKeyPressed(Keyboard::R))
+        {
+            gamestate = mainmenu;
+            MainMenuMusic.play();
+        }
     }
 }
 void Draw()
@@ -159,22 +246,39 @@ void Draw()
 
     if (gamestate == mainmenu)
     {
+        ////Debugging
+        //window.draw(StartButton);
+        //window.draw(SettingsButton);
+        //window.draw(LeaderboardButton);
+        //window.draw(ExitButton);
+         
+
         // main menu draw
-        window.draw(MainMenuBackGround);
-        // window.draw(StartButton);
+        window.draw(MainMenuBackground);
+        window.draw(MainMenuButtons);
         window.draw(StartGameText);
+        window.draw(SettingsText);
+        window.draw(LeaderboardText);
+        window.draw(ExitText);
+
     }
     if (gamestate == gameloop)
     {
         // gameloop draw
+
+
     }
     if (gamestate == settings)
     {
         // settings menu draw
+
+
     }
     if (gamestate == gameover)
     {
         // gameover screen draw
+
+
     }
 
     window.display(); // Display sprites on screen
